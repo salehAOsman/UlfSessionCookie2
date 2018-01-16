d8588e40e4e7e01bd7a1a2da98e553071ed7bd54
Saleh Osman <saleh1.a.osman@gmail.com>
2018-01-16 15:03:18 +0100

    advance Cookie and session by Ulf

---
+++ WebSessCookie/Controllers/HighScoreController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebSessCookie.Controllers
+{
+    public class HighScoreController : Controller
+    {
+        // GET: HighScore
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(string number)
+        {
+            HttpCookie myCookie= Request.Cookies["hight"];
+            //1 give key word, we make object for cookie
+            if (myCookie==null)
+            {
+            myCookie = new HttpCookie("hight");
+            myCookie["values"] = number;//
+            }
+            else
+            {
+                myCookie["values"] = myCookie["values"] + '£'+ number;
+            }
+            myCookie.Expires = DateTime.Now.AddHours(1);//add time 
+            Response.Cookies.Add(myCookie);//save cookie
+
+            var myNumbers = myCookie["values"].Split('£');
+
+            ViewBag.MyNumbers = myNumbers;
+
+            return View();
+        }
+    }
+}
---
+++ WebSessCookie/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebSessCookie.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(string name)
+        {
+            List<string> names;
+            if (Session["Names"]==null)
+            {
+                names=new List<string>();
+            }
+            else
+            {
+               names=(List<string>)Session["Names"];
+            }
+
+            names.Add(name);
+            Session["Names"] = names;
+            return View();
+        }
+    }
+}
